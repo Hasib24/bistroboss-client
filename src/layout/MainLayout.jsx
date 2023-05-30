662195b19304903cc4bcc59af4dd38a0070dc01e
Hasib24 <hasibislam1488@gmail.com>
2023-05-30 18:45:35 +0600

    add nav bar

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from '../pages/home/Home';
 import Footer from '../pages/shared/Footer';
+import NavBar from '../pages/shared/navbar/NavBar';
 
 const MainLayout = () => {
     return (
         <main>
+            <NavBar></NavBar>
             <Home></Home>
             <Footer></Footer>
         </main>
