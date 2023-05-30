00cdf13b8eba73b8154ad063432a2195810edb0d
Hasib24 <hasibislam1488@gmail.com>
2023-05-30 18:28:24 +0600

    initaial commit

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../pages/home/Home';
+import Footer from '../pages/shared/Footer';
+
+const MainLayout = () => {
+    return (
+        <main>
+            <Home></Home>
+            <Footer></Footer>
+        </main>
+    );
+};
+
+export default MainLayout;
