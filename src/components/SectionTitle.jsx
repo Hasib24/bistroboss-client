1892bd277d9a787d9ab53ebc555b42ca2d84dcb6
Hasib24 <hasibislam1488@gmail.com>
2023-05-31 00:15:12 +0600

    add popular menu

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SectionTitle = ({children}) => {
+    return (
+        <div className='text-center my-5 text-3xl font-semibold'>
+            {children}
+        </div>
+        
+    );
+};
+
+export default SectionTitle;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './banner/Banner';
 import SwiperCategory from '../swiper/SwiperCategory';
+import PopularMenu from '../popularmenu/PopularMenu';
 
 
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <SwiperCategory></SwiperCategory>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
