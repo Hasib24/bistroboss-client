825004ca73ea728ab8b2d479c1a99c25573f4abc
Hasib24 <hasibislam1488@gmail.com>
2023-05-30 23:36:13 +0600

    add swiper

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from './banner/Banner';
+import SwiperCategory from '../swiper/SwiperCategory';
+
+
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <SwiperCategory></SwiperCategory>
     </div>
   );
 };
