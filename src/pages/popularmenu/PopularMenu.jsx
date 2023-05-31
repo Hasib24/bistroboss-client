2e69f45f39085dc7d9b84b70f437a9522e8deff5
Hasib24 <hasibislam1488@gmail.com>
2023-05-31 08:37:37 +0600

    use coustom hook to fetch data

--- src/pages/popularmenu/PopularMenu.jsx
+++ src/pages/popularmenu/PopularMenu.jsx
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../components/SectionTitle';
 import MenuItem from '../shared/menuItem/MenuItem';
+import useMenu from '../../hooks/useMenu';
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([])
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=> res.json())
-        .then(data =>{
-            const popularItems = data.filter(item=>item.category=='popular')
-            setMenu(popularItems)
-        })
-    }, [])
+    const [menu] =useMenu()
+    const popularItems = menu.filter(item=>item.category=='popular')
+    
     return (
         <section className='container mx-auto my-10'>
             <SectionTitle>Popular Menu</SectionTitle>
             <div className='grid md:grid-cols-2 gap-4'>
-                {menu.map(item=><MenuItem key={item._id} item={item}></MenuItem>)}
+                {popularItems.map(item=><MenuItem key={item._id} item={item}></MenuItem>)}
             </div>
             <div className='text-center'>
                 <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
