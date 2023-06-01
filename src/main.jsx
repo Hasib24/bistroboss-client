b3b619509cb012f0406f694cfedc522dbc22b262
Hasib24 <hasibislam1488@gmail.com>
2023-06-01 14:22:47 +0600

    add context api / AuthcontextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Router'
+import AuthContextProvider from './providers/AuthContextProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}></RouterProvider>
+  <AuthContextProvider>
+    <RouterProvider router={router}></RouterProvider>
+  </AuthContextProvider>
 )
---
+++ src/providers/AuthContextProvider.jsx
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+
+import { getAuth } from "firebase/auth";
+import { app } from '../../firebase.config';
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+
+export const AuthContex = createContext(null)
+
+const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loader, setLoader] = useState(true);
+
+
+
+    // const createUser = (email, pass) =>{
+    //     return createUserWithEmailAndPassword(auth, email, pass)
+    // }
+
+    // const signInUser = (email, pass) =>{
+    //     return signInWithEmailAndPassword(auth, email, pass)
+    // }
+
+    // const googleSignIn = () =>{
+    //     return signInWithPopup(auth, googleProvider)
+    // }
+    // const logOut = () =>{
+    //         return signOut(auth)
+    // }
+
+    // useEffect(()=>{
+    //     const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+    //         setUser(currentUser)
+    //         setLoader(null)
+    //     })
+
+    //     //stop observing after unmount
+    //     return unsubscribe()
+    // },[])
+
+    // const userInfo ={
+    //     user,
+    //     setUser,
+    //     loader,
+    //     setLoader,
+    //     toys,
+    //     setToys,
+    //     createUser,
+    //     signInUser,
+    //     googleSignIn,
+    //     logOut
+    // }
+
+    const userInfo ={
+        user,
+        setUser,
+        loader,
+        setLoader
+
+    }
+
+    return (
+        <AuthContex.Provider value={userInfo}>
+            {children}
+        </AuthContex.Provider>
+    );
+};
+
+export default AuthContextProvider;
