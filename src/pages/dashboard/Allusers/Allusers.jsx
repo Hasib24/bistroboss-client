d55e6262c4cd06533cb3b6d891ada9ab1771fa8b
Hasib24 <hasibislam1488@gmail.com>
2023-06-03 22:44:49 +0600

    load user using tanstack react query

--- src/pages/dashboard/Allusers/Allusers.jsx
+++ src/pages/dashboard/Allusers/Allusers.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+
+
+
 
 const Allusers = () => {
+
+
+    const {data : users = [], refetch } = useQuery(['users'], async ()=>{
+        const res = await fetch('http://localhost:5000/users')
+        return res.json()
+    })
+
+
     return (
         <div>
-            all users
+            all users {users.length}
         </div>
     );
 };
